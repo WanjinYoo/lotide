e7c53698aef3351b3695029ebf702d31394bd535
Wanjin Yoo <dksladjsla@gmail.com>
2020-10-28 06:23:05 +0000

    flatten added

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const flatten = function(array) {
+  let newarray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newarray.push(array[i][j]);
+      }
+    } else {
+      newarray.push(array[i]);
+    }
+  }
+  return newarray;
+};
+
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
