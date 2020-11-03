3e56556d5c0b3085cde274ad9afcc54fb0936cdb
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-03 01:40:40 +0000

    headTest has been modified, Mocha & Chai are used to test the function

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😩Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(array1,array2) {
   if (array1.length !== array2.length) {
@@ -13,8 +6,7 @@ const eqArrays = function(array1,array2) {
     for (let i = 0; i < array1.length; i++) {
       if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
         if (!eqArrays(array1[i],array2[i])) return false;
-      }
-      else if (array1[i] !== array2[i]) {
+      } else if (array1[i] !== array2[i]) {
         return false;
       }
     }
@@ -22,11 +14,4 @@ const eqArrays = function(array1,array2) {
   }
 };
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-const assertArraysEqual = function(arr1,arr2) {
-  (eqArrays(arr1,arr2)) ? console.log(`😃Assertion passed: ${arr1} === ${arr2}`)
-    : console.log(`😩Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
 const middle = function(array) {
   const resultarray = [];
   array.length < 3 ? resultarray.push()
@@ -22,8 +5,5 @@ const middle = function(array) {
   : resultarray.push(array[Math.floor(array.length / 2) - 1],array[Math.floor(array.length / 2)]);
   return resultarray;
 };
+module.exports = middle;
 
-
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
