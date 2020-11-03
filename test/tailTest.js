517dd290d499fcd0c54f11adb0ed5d584d314d70
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-03 02:20:00 +0000

    exports 3 functions simultaneously

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,10 @@
-const assertEqual = require(`../assertEqual`);
+const assert = require('chai').assert;
 const tail = require(`../tail`);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#Tail", () => {
+  it("returns elements excluding the head for [ Yo Yo, Lighthouse, Labs]", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words),["Lighthouse","Labs"]);
+  });
+});
+
+
